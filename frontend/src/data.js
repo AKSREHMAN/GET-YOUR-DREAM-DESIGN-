5c52f6500d6262428647f6e33b82f05ca67efd05
AKSREHMAN <mohdaksjaved@gmail.com>
2021-07-27 22:51:39 +0530

    share code on github

---
+++ frontend/src/data.js
@@ -0,0 +1,52 @@
+const data = {
+    products: [
+      {
+        _id: '1',
+        name: 'Food Cutting Board',
+        category: 'Household',
+        image: '/images/p1.jpg',
+        price: 350,
+        brand: 'GYDD',
+        rating: 4.5,
+        numReviews: 10,
+        description: 'Design Girl With Vegetables',
+      },
+      {
+        _id: '2',
+        name: 'Chopping Board',
+        category: 'Household',
+        image: '/images/p2.jpg',
+        price: 349,
+        brand: 'GYDD',
+        rating: 4.5,
+        numReviews: 10,
+        description: 'Engraving Basket With Vegetables',
+      },
+      {
+        _id: '3',
+        name: 'Tree On Food',
+        category: 'Household',
+        image: '/images/p3.jpg',
+        price: 390,
+        brand: 'GYDD',
+        rating: 4.5,
+        numReviews: 10,
+        description: ' Cutting Board high quality product',
+      },
+      {
+        _id: '4',
+        name: 'Wooden Chopping Board',
+        category: 'Household',
+        image: '/images/p4.jpg',
+        price: 355,
+        brand: 'GYDD',
+        rating: 4.5,
+        numReviews: 10,
+        description: 'Design Vegetables high quality product',
+      },
+     
+    ],
+
+
+};
+export default data;
